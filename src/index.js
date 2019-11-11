5426df45928490b9d0abe2d6b34b3f2898cc3874
Victor K Varghese <victorvarghese148@gmail.com>
2019-11-11 08:39:48 +0530

    watermelon messenger first code commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class Entrypoint extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Entrypoint </Text>
+      </View>
+    );
+  }
+}
