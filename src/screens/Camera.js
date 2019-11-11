ea55bbee9c5e3a9451024c9a4d6647779416a795
Victor K Varghese <victorvarghese148@gmail.com>
2019-11-11 20:30:28 +0530

    added camera module

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -1,18 +1,33 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 import {withNavigationFocus} from 'react-navigation';
 
 class Camera extends Component {
+  state = {
+    isCameraShown: false,
+  };
+
   render() {
     return (
       <View
         style={{
           flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: this.props.isFocused ? 'red' : 'black',
+          flexDirection: 'column',
+          backgroundColor: 'black',
         }}>
-        <Text> Camera </Text>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={{flex: 1, flexDirection: 'column', backgroundColor: 'black'}}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={'Permission to use camera'}
+          permissionDialogMessage={
+            'We need your permission to use your camera phone'
+          }
+        />
       </View>
     );
   }
